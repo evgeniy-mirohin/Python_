85eedec506ae8c02308c13cae5fc099b6ff4c0ce
Мирохин Евгений Владимирович <mirohinev@spb.katren.ru>
2020-12-03 17:15:38 +0300

    сохраняю данные в файл

--- robot-tasks-master/LesonPython/AD/AD_GUI3.py
+++ robot-tasks-master/LesonPython/AD/AD_GUI3.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3.6
-# -*- coding: utf-8 -*-
 import getpass
 import ldap3
 from pprint import pprint
 from tkinter import *
 import sys
+from tkinter import messagebox
     #############Переменные##
 ad_name='OU=Users,OU=spb,OU=KATREN,DC=katren,DC=net'
 server_win_uri = 'katren.net'
@@ -27,6 +26,31 @@ root.geometry("400x600")        # задаем размеры окна
 message = StringVar()   # чтобы заработал (.get)
 # # # # # # # # # # # # # # # # #
 
+#######Классы#############
+class windows:
+    def __init__(self):
+        top=self.top=Toplevel()
+        top.title("введите пароль")
+        top.geometry("200x100")
+        l=Label(top,text="введите пароль")
+        l.pack()
+        e=Entry(top, textvariable=message)
+        e.pack()
+        b=Button(top,text='Ok',command = lambda:self.cleanup())
+        b.pack()
+
+    def cleanup(self):
+    #message=e.get()
+        #print("rtyuui")
+        self.top.destroy()
+        #top.destroy() 
+    #top.destroy() 
+       # print (message.get())
+
+# # # # # # # # # #  # # # #  # # 
+
+
+
     ### Функции ######
 def get_users_win_data(ip,search_base,search_filter,attrs,win_bind_name,win_bind_passwd):
     server = ldap3.Server('ldap://{}'.format(ip))
@@ -34,22 +58,17 @@ def get_users_win_data(ip,search_base,search_filter,attrs,win_bind_name,win_bind
         conn.search(search_base, search_filter, attributes=attrs)
         return(conn.entries)
 
-def GO():
-    top=Toplevel()
-    top.title("введите пароль")
-    top.geometry("200x100")
-    l=Label(top,text="введите пароль")
-    l.pack()
-    e=Entry(top, textvariable=message)
-    e.pack()
-    b=Button(top,text='Ok',command=cleanup)
-    b.pack()
+def write1():
+    #print (message.get())
+    result=get_users_win_data(server_win_uri,ad_name,search_filter,attrs,win_bind_name,message.get())
+   # messagebox.showinfo("GUI Python", len(result))
+    En1.insert(0,len(result))
+    #En1["text"] = "reter"
+    f1 = open("AD_Result.txt", 'w')
+    f1.write()
+    f1.close()
 
-def cleanup():
-    #message=e.get()
-    #top.destroy() 
-     
-    print (message.get())
+
       
 # # # # # # # # # #
 
@@ -61,11 +80,13 @@ L2 = Label(text="введите пароль")
 L2.place(relx=.2, rely=.15)
 En1 = Entry(justify=RIGHT)
 En1.place(relx=.5, rely=.1, anchor="c")
+#En1.insert(0,"ntcn")
 EnPasswd = Entry(justify=RIGHT)
 EnPasswd.place(relx=.5, rely=.2, anchor="c")
-btnGO = Button(text="GO",  command=lambda: GO())
+btnGO = Button(text="GO",  command=lambda: windows())
 btnGO.place(relx=.6, rely=.3)
-btnPrint = Button(text="Print",  command = pprint(data2))
+#btnPrint = Button(text="Print",  command = lambda:print(get_users_win_data(server_win_uri,ad_name,search_filter,attrs,win_bind_name,message.get())))
+btnPrint = Button(text="Print",  command = lambda:write1())
 btnPrint.place(relx=.5, rely=.3)
 
 
@@ -76,5 +97,5 @@ root.mainloop()
 
  
   
-data2=get_users_win_data(server_win_uri,ad_name,search_filter,attrs,win_bind_name,message.get())
+#data2=get_users_win_data(server_win_uri,ad_name,search_filter,attrs,win_bind_name,message.get())
 #pprint(data2)
